f65f6f6c12675851ce8cbd6c4557c70b7f015ca8
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2022-11-22 22:16:16 +0530

    Designed Skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,25 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='bg-dark h-screen'>
+      <div className='flex' Style='height: 88vh;'>
+        <div className='w-56 bg-black h-full flex-none'>
+          <h1 className="text-3xl bg-green font-bold underline">
+            Hello world!
+          </h1>
+        </div>
+        <div className='w-full h-full relative'>
+          <div className='w-full stickey top-0 p-2'>
+            
+          </div>
+        </div>
+      </div>
+      <div className='w-full bg-light' Style='height: 12vh;'>
+
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: 'Proxima Nova',
+    container: {
+      center: true
+    },
+    extend: {
+      colors: {
+        'green': '#1DB954',
+        'dark': '#121212',
+        'Darkest': '#191414',
+        'light': '#282828',
+        'lightest': '#B3B3B3',
+      }
+    },
   },
   plugins: [],
 }
