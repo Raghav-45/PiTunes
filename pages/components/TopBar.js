25cba7b996bc5fa1857a497257b3458a570361dd
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2022-11-26 21:58:07 +0530

    Organizing Things

---
+++ pages/components/TopBar.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+import { AiFillHome, AiFillHeart, AiOutlineArrowDown } from "react-icons/ai"
+import { FaPlay, FaPause } from "react-icons/fa"
+import { BiSearchAlt, BiLibrary, BiShuffle } from "react-icons/bi"
+import { BsFillPlusSquareFill } from "react-icons/bs"
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight, MdKeyboardArrowUp, MdKeyboardArrowDown, MdDevicesOther } from "react-icons/md"
+import { RiPictureInPictureFill, RiPlayListFill } from "react-icons/ri"
+import { IoMdArrowDropup, IoMdArrowDropdown } from "react-icons/io"
+import { HiVolumeUp } from "react-icons/hi"
+import { BsFillSkipStartFill, BsFillSkipEndFill } from "react-icons/bs"
+import { FiRepeat } from "react-icons/fi"
+
+const TopBar = () => {
+  const ProfilePic = '/assets/unnamed.jpg'
+  const [ShowDropdown, setShowDropdown] = useState(false)
+
+  return (
+    <div className='w-full sticky top-0 py-4 px-6 flex items-center justify-between bg-white/80 dark:bg-dark/80 z-10'>
+      <div className="flex h-8 w-20 items-center justify-between">
+        <button className='rounded-full bg-gray-100 dark:bg-black w-8 h-8 text-gray-500 dark:text-white opacity-50'>
+          <MdKeyboardArrowLeft className='text-3xl'/>
+        </button>
+        <button className='rounded-full bg-gray-100 dark:bg-black w-8 h-8 text-gray-500 dark:text-white'>
+          <MdKeyboardArrowRight className='text-3xl'/>
+        </button>
+      </div>
+      <div className='relative'>
+        <button onClick={() => setShowDropdown(!ShowDropdown)} className='focus:outline-none bg-gray-100 dark:bg-light rounded-full h-auto py-1 px-1 w-auto flex items-center'>
+          <img src={ProfilePic} className='rounded-full h-6 w-6 mr-2'></img>
+          <p className='text-gray-900 dark:text-white font-semibold text-xs mr-2'>Raghav</p>
+          {ShowDropdown ? <MdKeyboardArrowUp className='text-gray-900 dark:text-white'/> : <MdKeyboardArrowDown className='text-gray-900 dark:text-white'/>}
+        </button>
+
+        {ShowDropdown && 
+          <div onClick={() => setShowDropdown(false)} className="absolute bg-gray-100 dark:bg-light w-full rounded mt-1">
+            <button onClick={() => setShowDropdown(false)} className='focus:outline-none w-full text-sm py-2 hover text-gray-900 dark:text-white opacity-75 hover:opacity-100'>Account</button>
+            <button onClick={() => setShowDropdown(false)} className='focus:outline-none w-full text-sm py-2 hover text-gray-900 dark:text-white opacity-75 hover:opacity-100'>Log Out</button>
+            {/* <label onClick={() => setIsDarkMode(!IsDarkMode)} className="inline-flex relative items-center cursor-pointer">
+              <input checked type="checkbox" value="" className="sr-only peer"/>
+              <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+              <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Dark</span>
+            </label> */}
+          </div>
+        }
+      </div>
+    </div>
+  )
+}
+
+export default TopBar
