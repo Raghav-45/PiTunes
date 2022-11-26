c0a47b137213296b647a638a44ffb5b91ffa202f
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2022-11-26 19:41:43 +0530

    Update index.js

--- pages/light/index.js
+++ pages/light/index.js
@@ -217,16 +217,16 @@ export default function Home() {
               <div className='w-full flex flex-wrap'>
                 {data.recents.map((elem) => (
                   <div key={elem.key} className="group p-2 w-48 relative">
-                    <div className="bg-light w-full h-auto p-4 rounded-lg shadow-md">
+                    <div className="bg-gray-300 w-full h-auto p-4 rounded-lg shadow-md">
                       {/* <div className='relative h-auto w-full shadow-[0_8px_24px_rgb(0,0,0,50%)] mb-3 rounded-md'> */}
                       <div className='relative h-36 w-full shadow-[0_8px_24px_rgb(0,0,0,50%)] mb-3 rounded-md'>
-                        <div className='absolute playButton bg-green rounded-full h-10 w-10 m-2 flex right-0 bottom-0 items-center justify-center transition opacity-0 group-hover:opacity-100 translate-y-7 group-hover:-translate-y-0'>
+                        <div className='absolute playButton bg-green-500 rounded-full h-10 w-10 m-2 flex right-0 bottom-0 items-center justify-center transition opacity-0 group-hover:opacity-100 translate-y-7 group-hover:-translate-y-0'>
                           <button onClick={() => {setAudio();}}><FaPlay className='text-white text-1xl'/></button>
                         </div>
                         <img src={elem.src} className='h-full w-full rounded-md' />
                       </div>
-                      <h1 className='text-sm font-semibold text-white tracking wide whitespace-nowrap overflow-x-hidden text-ellipsis'>{elem.title}</h1>
-                      <h2 className='text-xs text-lightest tracking-wide pb-5'>{elem.artist}</h2>
+                      <h1 className='text-sm font-semibold text-gray-900 tracking wide whitespace-nowrap overflow-x-hidden text-ellipsis'>{elem.title}</h1>
+                      <h2 className='text-xs text-gray-500 tracking-wide pb-5'>{elem.artist}</h2>
                     </div>
                   </div>
                 ))}
@@ -243,7 +243,7 @@ export default function Home() {
                   <div key={elem.id} className="group p-2 w-48 relative">
                     <div className="bg-light w-full h-auto p-4 rounded-lg shadow-md">
                       <div className='relative h-36 w-full shadow-[0_8px_24px_rgb(0,0,0,50%)] mb-3 rounded-full'>
-                        <div className='absolute playButton bg-green rounded-full h-10 w-10 m-2 flex right-0 bottom-0 items-center justify-center transition opacity-0 group-hover:opacity-100 translate-y-7 group-hover:-translate-y-0'>
+                        <div className='absolute playButton bg-green-500 rounded-full h-10 w-10 m-2 flex right-0 bottom-0 items-center justify-center transition opacity-0 group-hover:opacity-100 translate-y-7 group-hover:-translate-y-0'>
                           <button><FaPlay className='text-white text-1xl'/></button>
                         </div>
                         <img src={elem.image} className='h-full w-full rounded-full' />
@@ -266,7 +266,7 @@ export default function Home() {
                   <div key={elem.id} className="group p-2 w-48 relative">
                     <div className="bg-light w-full h-auto p-4 rounded-lg shadow-md">
                       <div className='relative h-36 w-full shadow-[0_8px_24px_rgb(0,0,0,50%)] mb-3 rounded-full'>
-                        <div className='absolute playButton bg-green rounded-full h-10 w-10 m-2 flex right-0 bottom-0 items-center justify-center transition opacity-0 group-hover:opacity-100 translate-y-7 group-hover:-translate-y-0'>
+                        <div className='absolute playButton bg-green-500 rounded-full h-10 w-10 m-2 flex right-0 bottom-0 items-center justify-center transition opacity-0 group-hover:opacity-100 translate-y-7 group-hover:-translate-y-0'>
                           <button><FaPlay className='text-white text-1xl'/></button>
                         </div>
                         <img src={elem.image} className='h-full w-full rounded-full' />
@@ -289,7 +289,7 @@ export default function Home() {
                   <div key={elem.key} className="group p-2 w-48 relative">
                     <div className="bg-light w-full h-auto p-4 rounded-lg shadow-md">
                       <div className='relative h-36 w-full shadow-[0_8px_24px_rgb(0,0,0,50%)] mb-3 rounded-md'>
-                        <div className='absolute playButton bg-green rounded-full h-10 w-10 m-2 flex right-0 bottom-0 items-center justify-center transition opacity-0 group-hover:opacity-100 translate-y-7 group-hover:-translate-y-0'>
+                        <div className='absolute playButton bg-green-500 rounded-full h-10 w-10 m-2 flex right-0 bottom-0 items-center justify-center transition opacity-0 group-hover:opacity-100 translate-y-7 group-hover:-translate-y-0'>
                           <button><FaPlay className='text-white text-1xl'/></button>
                         </div>
                         <img src={elem.src} className='h-full w-full rounded-md' />
@@ -313,7 +313,7 @@ export default function Home() {
                     <div className="bg-light w-full h-auto p-4 rounded-lg shadow-md">
                       {/* <div className='relative h-auto w-full shadow-[0_8px_24px_rgb(0,0,0,50%)] mb-3 rounded-md'> */}
                       <div className='relative h-36 w-full shadow-[0_8px_24px_rgb(0,0,0,50%)] mb-3 rounded-md'>
-                        <div className='absolute playButton bg-green rounded-full h-10 w-10 m-2 flex right-0 bottom-0 items-center justify-center transition opacity-0 group-hover:opacity-100 translate-y-7 group-hover:-translate-y-0'>
+                        <div className='absolute playButton bg-green-500 rounded-full h-10 w-10 m-2 flex right-0 bottom-0 items-center justify-center transition opacity-0 group-hover:opacity-100 translate-y-7 group-hover:-translate-y-0'>
                           <button onClick={() => {setAudio(elem.downloadUrl[elem.downloadUrl.length-1].link);}}><FaPlay className='text-white text-1xl'/></button>
                         </div>
                         <img src={elem.image[1].link} className='h-full w-full rounded-md' />
