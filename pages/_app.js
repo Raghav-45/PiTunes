3530a4df9443452de2630db3f06e1482745f2791
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2022-11-27 15:46:43 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<>
+      <Script
+        id="Adsense-id"
+        async
+        onError={(e) => { console.error("Script failed to load", e); }}
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5588726027217390"
+        crossorigin="anonymous"/>
+      
+      <Component {...pageProps} />
+  </>)
 }
 
 export default MyApp
