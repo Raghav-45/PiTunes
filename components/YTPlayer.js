8b22e58c1e364f77d1315b55d969dcc17454c87c
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-01-27 17:14:53 +0530

    Update YTPlayer.js

--- components/YTPlayer.js
+++ components/YTPlayer.js
@@ -22,7 +22,7 @@ export const YTPlayer = ((props) => {
   return (
     // load the YouTube player
     // <YouTube videoId={MusicSourceURL} opts={opts} onReady={onYTReady} />
-    <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+    <ReactPlayer url={`https://www.youtube.com/watch?v=${MusicSourceURL}`} />
   )
 })
 YTPlayer.displayName = 'YTPlayer'
