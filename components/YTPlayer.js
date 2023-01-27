77b9e8fc7516002c4ce4ef1d150a3e64da326873
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-01-27 17:11:32 +0530

    Update YTPlayer.js

--- components/YTPlayer.js
+++ components/YTPlayer.js
@@ -1,21 +1,28 @@
 import YouTube from 'react-youtube'
 import { PlayerContext } from '../contexts/ContextApi'
+import ReactPlayer from 'react-player/youtube'
 
 export const YTPlayer = ((props) => {
   const VideoID = props.VideoID
-  const { MusicSourceURL } = PlayerContext()
+  const { MusicSourceURL, YTPlayer, setYTPlayer, IsPlaying } = PlayerContext()
 
   const opts = {
     height: '72',
     width: '128',
     playerVars: {
       autoplay: 1,
-    }
+    },
+  }
+
+  const onYTReady = (e) => {
+    // access to player in all event handlers via event.target
+    e.target.pauseVideo();
   }
 
   return (
     // load the YouTube player
-    <YouTube videoId={MusicSourceURL} opts={opts} onReady={(e) => e.target.pauseVideo()} />
+    // <YouTube videoId={MusicSourceURL} opts={opts} onReady={onYTReady} />
+    <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
   )
 })
 YTPlayer.displayName = 'YTPlayer'
