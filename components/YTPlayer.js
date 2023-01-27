1ee4613ca77c8c44c6999762d44d9d12d16c0e84
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-01-27 18:17:24 +0530

    Update YTPlayer.js

--- components/YTPlayer.js
+++ components/YTPlayer.js
@@ -19,7 +19,7 @@ export const YTPlayer = ((props) => {
 
   return (
     // load the YouTube player
-    <ReactPlayer url={`https://www.youtube.com/watch?v=${MusicSourceURL}`} playing={IsPlaying} controls={true} onDuration={(e) => setMusicDuration(e)} muted={false} onProgress={(e) => setMusicCurrentTime(e.playedSeconds)} />
+    <ReactPlayer url={'https://music.youtube.com/watch?v=_cmORZMgv6I'} playing={IsPlaying} controls={true} onDuration={(e) => setMusicDuration(e)} muted={false} onProgress={(e) => setMusicCurrentTime(e.playedSeconds)} />
   )
 })
 YTPlayer.displayName = 'YTPlayer'
