1629af5c1dd546a1a1d22491a09dbf3fd44d3a0a
Aditya <raghavbhai4545@gmail.com>
2024-08-01 20:43:17 +0530

    reinstall

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: false // it should be false by default
-}
-
-module.exports = nextConfig
--- pages/index.js
+++
@@ -1,7 +0,0 @@
-import HomePage from './home'
-
-export default function Home() {
-  return (
-    <HomePage/>
-  )
-}
