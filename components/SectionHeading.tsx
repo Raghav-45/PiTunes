b82ef16782e8117ccdb3beb23bf17f539ff22d88
Aditya <raghavbhai4545@gmail.com>
2024-08-05 12:07:22 +0530

    Update SectionHeading.tsx

--- components/SectionHeading.tsx
+++ components/SectionHeading.tsx
@@ -2,12 +2,16 @@ import { FC } from 'react'
 
 interface SectionHeadingProps {
   name: string
+  extra?: string
 }
 
-const SectionHeading: FC<SectionHeadingProps> = ({ name }) => {
+const SectionHeading: FC<SectionHeadingProps> = ({ name, extra }) => {
   return (
     <div className="flex items-center justify-between tracking-wider">
-      <h1 className="pl-2 text-2xl text-white font-semibold">{name}</h1>
+      <div className="pl-2 flex flex-col">
+        <h1 className="text-2xl text-white font-semibold">{name}</h1>
+        {extra && <h2 className="text-lightest">{extra}</h2>}
+      </div>
       <h2 className="pr-4 text-xs text-lightest self-end uppercase mb-1.5">
         See All
       </h2>
